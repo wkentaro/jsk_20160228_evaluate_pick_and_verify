1b1d60c697776ab644f116a8cd71d55e28caaad0
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-28 16:15:22 +0900

    Update as stripplot

--- elapsed_time.py
+++ elapsed_time.py
@@ -13,6 +13,14 @@ time_cols = [col for col in df.columns if col.startswith('time')]
 df_time = df[time_cols]
 df_time_ratio = df_time.div(df_time.sum(axis=1), axis='index')
 df_time_ratio.columns = [col.split('time_')[-1] for col in time_cols]
-sns.boxplot(x=df_time_ratio, orient='h')
-plt.xlabel('Time (ratio to pick and place/return execution)')
+time_rows = []
+for i, row in df_time_ratio.iterrows():
+    for action in ['detect', 'pick', 'verify', 'place', 'return']:
+        time_rows.append((action, row[action]))
+
+df = pd.DataFrame(time_rows)
+df.columns = ['action', 'time']
+sns.stripplot(y='action', x='time', data=df, jitter=True, linewidth=1)
+plt.ylabel('Part of Task')
+plt.xlabel('Time (Ratio to Whole Task Execution)')
 plt.show()
